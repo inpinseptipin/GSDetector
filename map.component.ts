0f142270797f188cc62f93eb368c753a024b4ca8
ms3na92 <158223088+ms3na92@users.noreply.github.com>
2024-02-19 15:11:15 -0500

    Add files via upload

---
+++ map.component.ts
@@ -0,0 +1,12 @@
+// map.component.ts
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent {
+  // Add logic for the map component here
+}
